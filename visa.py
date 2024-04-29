abd43696811d58c5fedeb7dc471cbb4306f8a221
nauroz <alinauroze@hotmail.com>
2024-04-29 17:21:09 +0500

    headless

--- visa.py
+++ visa.py
@@ -282,6 +282,7 @@ if LOCAL_USE:
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--user-data-dir=/home/ezee/temp")
     chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), 
         options=chrome_options
